00ca034f1c5aae31ec1b84e5b66c1568066ceb01
Jack Edge <yellowbounder@gmail.com>
2011-02-09 22:56:20 +0000

    Created framework skeleton of universe module
    Modules are good. As such, we'll start with the universe module,
    which should be responsible for keeping track of everything in the
    universe. All the fake stars, and the ships, and so on.

---
+++ universe/universe.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__doc__="""The universe in sidereal is the game space, it is the space
+in which all ingame object occupy. It should keep track of all in-game
+related objects, such as ships, explosions, projectiles, and so on, or be
+able to keep track of them on demand."""
+
+class Universe(object):
+    def __init__(self):
+        pass
