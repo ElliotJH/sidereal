a36ac31faeef0c409c1a54979d66a78e6a239bd7
Jack Edge <yellowbounder@gmail.com>
2011-02-09 23:22:41 +0000

    Created universe.stars module for star generation
    Created the stars.py file, which should, currently in the plan,
    create a large number of stars on a sphere surface, and return
    the corresponding NodeObject.
    
    That's the plan anyway. We'll see how it turns out.

---
+++ universe/stars.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# You know, I'm not sure if it's worth putting the shabang in, considering
+# that these modules *shouldn't* be run directly. Good for debugging though
+# I guess.
+
+# Now in our universe, there need to be stars. As such there should be a quick
+# and easy method of including them in the scene graph.
+
+import direct.directbase.DirectStart
