4a69b2f44b915880816724507a6a8e610cb1445b
Jack Edge <yellowbounder@gmail.com>
2011-02-12 22:36:29 +0000

    Finally got the teapot to display
    It wasn't lighting, it turns out it was camera position. After writing a
    little Task, to randomly change the camera position, I found a location
    that worked. Then I realised you could just have the camera lookAt() the
    teapot.
    
    However, this is very much NOT the loading screen, so I'm going to move
    it next commit.

--- graphics/screen.py
+++ graphics/screen.py
@@ -1,4 +1,8 @@
-"""The code for the basic display, I guess."""
+"""The code for the basic display, I guess.
+
+No scrap that, this is pretty hacked together code for showing
+a flipping rotating teapot. We should totally include this in the
+credits. XD"""
 
 # The basic unit for the graphics in Panda3d is ShowBase, which contains
 # the render tree, and the task manager and so on.
@@ -6,16 +10,20 @@
 # As such, our class needs a reference to it, to do ANYTHING.
 
 import panda3d.core
+import direct.task
 import direct.gui.DirectGui
+
+import random
 
 class LoadingScreen(object):
     def __init__(self,showbase):
         self.base = showbase
         self.background()
 
+        self.teapot()
+
         self.lights()
         self.camera()
-        self.teapot()
         self.onscreen_text()
         self.action()
 
@@ -23,11 +31,11 @@ class LoadingScreen(object):
         # load the model, and attach it to the render tree
         self.teapot = self.base.loader.loadModel('models/teapot')
         self.teapot.reparentTo(self.base.render)
-        self.teapot.setPos(0,-20,-10)
+        self.teapot.setPos(0,0,0)
 
         self.teapot_movement = self.teapot.hprInterval(50,(0,360,360))
         self.teapot_movement.loop()
-        self.teapot.setColor(1,0,0,1)
+        self.teapot.setColor(1,1,1,1)
     def background(self):
         self.base.setBackgroundColor(0,0,0,1)
     def onscreen_text(self):
@@ -45,24 +53,36 @@ class LoadingScreen(object):
 
     def lights(self):
         # make our directional lightnode, and attach it to the render tree
-        dlightnode = panda3d.core.DirectionalLight('light')
-        dlightnode.setColor((0.8,0.7,0.6,1))
-        dlight = self.base.render.attachNewNode(dlightnode)
+        dlight = panda3d.core.DirectionalLight('light')
+        dlight.setColor((0.2,0.2,0.8,1))
+        dlight_nodepath = self.base.render.attachNewNode(dlight)
         # by setting dlight to light "render", it also lights all nodes
         # attached to render's subtree, as in, everything
-        self.base.render.setLight(dlight)
+        self.base.render.setLight(dlight_nodepath)
 
         # same for ambient light
         alightnode = panda3d.core.AmbientLight('ambient')
         alightnode.setColor((0.2,0.2,0.2,1))
         alight = self.base.render.attachNewNode(alightnode)
         self.base.render.setLight(alight)
+        dlight_nodepath.lookAt(self.teapot)
     def camera(self):
         self.base.camLens.setNearFar(1.0,10000)
         self.base.camLens.setFov(75)
+        self.base.camera.setPos(0,0,50)
+        self.base.camera.setHpr(0,-90,0)
         self.base.disableMouse()
+    def randomise_camera(self,task):
+        coords = []
+        for i in range(3):
+            coords.append(random.randint(-50,50))
+        print coords
+        self.base.camera.setPos(tuple(coords))
+        self.base.camera.lookAt(self.teapot)
+        return direct.task.Task.again
+
     def action(self):
-        pass
+        self.base.taskMgr.doMethodLater(0.5,self.randomise_camera,'randomise')
 
 if __name__=='__main__':
     # This is mostly debugging test stuff, and will not be run when
