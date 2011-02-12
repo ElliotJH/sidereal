e099e9be89ffa6514585dfb51dda4366f4e00306
Jack Edge <yellowbounder@gmail.com>
2011-02-12 20:05:01 +0000

    More screen faffer, can't get the teapot to show
    Feeling annoyed about why I can't get the teapot to display. Trying
    to get it to display so I know how to get models into the game.
    
    Got an overlay text to display though.

--- graphics/screen.py
+++ graphics/screen.py
@@ -6,6 +6,7 @@
 # As such, our class needs a reference to it, to do ANYTHING.
 
 import panda3d.core
+import direct.gui.DirectGui
 
 class LoadingScreen(object):
     def __init__(self,showbase):
@@ -15,6 +16,7 @@ class LoadingScreen(object):
         self.lights()
         self.camera()
         self.teapot()
+        self.onscreen_text()
         self.action()
 
     def teapot(self):
@@ -25,8 +27,22 @@ class LoadingScreen(object):
 
         self.teapot_movement = self.teapot.hprInterval(50,(0,360,360))
         self.teapot_movement.loop()
+        self.teapot.setColor(1,0,0,1)
     def background(self):
         self.base.setBackgroundColor(0,0,0,1)
+    def onscreen_text(self):
+        # ugh, indenting to ensure <80 chars
+        self.text = direct.gui.DirectGui.OnscreenText("BOOM",
+                                      style=1,
+                                      fg=(1,1,1,1),
+                                      mayChange=1,
+                                      pos=(-1.3,0.80),
+                                      align=panda3d.core.TextNode.ALeft,
+                                      scale = 0.05,
+                                      shadow=(0,0,0,1),
+                                      shadowOffset=(0.1,0.1)
+                                                     )
+
     def lights(self):
         # make our directional lightnode, and attach it to the render tree
         dlightnode = panda3d.core.DirectionalLight('light')
@@ -37,13 +53,14 @@ class LoadingScreen(object):
         self.base.render.setLight(dlight)
 
         # same for ambient light
-        alightnode = panda3d.core.DirectionalLight('ambient')
+        alightnode = panda3d.core.AmbientLight('ambient')
         alightnode.setColor((0.2,0.2,0.2,1))
         alight = self.base.render.attachNewNode(alightnode)
         self.base.render.setLight(alight)
     def camera(self):
         self.base.camLens.setNearFar(1.0,10000)
         self.base.camLens.setFov(75)
+        self.base.disableMouse()
     def action(self):
         pass
 
