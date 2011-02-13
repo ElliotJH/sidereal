19f7f7df6e00a72c90b14fdd88d72603671717e1
Jack Edge <yellowbounder@gmail.com>
2011-02-13 14:19:56 +0000

    Added FocusManager, a utility class for focusing
    "Focusing" or having focus on multiple items means that the camera
    pivots and swivels around a central point, and that point is the average
    of all objects which are focused. The FocusManager is a set which you
    add items to be focused to, and will tell the camera where to look.

--- interface/mainview.py
+++ interface/mainview.py
@@ -3,6 +3,8 @@ of the spaceships and stuff around.
 
 TODO this doc is clearly incomplete."""
 
+import collections
+
 # One thing I'm wondering about, is whether we implement the gui overlay
 # in a seperate space. How does it intercept the clicks, and know which one
 # is which? I guess we hope for panda magic.
@@ -20,3 +22,48 @@ TODO this doc is clearly incomplete."""
 class MainView(object):
     def __init__(self,showbase):
         self.base = showbase
+
+class FocusManager(collections.MutableSet):
+    """The FocusManager the utility for maintaining focus on one to many
+    game objects, with none being a special case.
+    
+    It is treated as a Set of objects, which should (TODO change to MUST) 
+    be ingame objects with coordinates. It can then determine the average
+    point to focus on from all of its component objects.
+    
+    If objects leave the FocusManager (such as by being destroyed, or 
+    de-focused, the FocusManager then recalculates its "average".
+    If all objects leave, then it will continue to stare at the last point
+    a la CalHomeworld behaviour."""
+    def __init__(self):
+        self._internal_set = set()
+        self.coord = (0,0,0)
+    def add(self,item):
+        self._internal_set.add(item)
+        self.coord = self._calculate_average()
+    def discard(self,item):
+        self._internal_set.discard(item)
+        # If len is 0, then it'll keep its last average_coord
+        if len(self) > 0:
+            self.coord = self._calculate_average()
+    def __len__(self):
+        return len(self._internal_set)
+    def __iter__(self):
+        return iter(self._internal_set)
+    def __contains__(self,item):
+        return item in self._internal_set
+    def _calculate_average(self):
+        """Assumes that all objects in the set have a .coord attribute"""
+        # AVerageX and so on.
+        avx,avy,avz = 0
+        for item in self:
+            avx += item.coord[0]
+            avy += item.coord[1]
+            avz += item.coord[2]
+
+        avx /= len(self)
+        avy /= len(self)
+        avz /= len(self)
+        return (avx,avy,avz)
+
+
