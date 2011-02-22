dedcd77074261c878421c89a9615631ba038c626
Jack Edge <yellowbounder@gmail.com>
2011-02-22 15:30:51 +0000

    Realised relative power of JSON, uses that now
    If we're going to be passing lists, and such the like around, we should
    use a format that has support for such things. This also makes it far
    more portable in the event we suddenly switch languages.

--- universe/ships.py
+++ universe/ships.py
@@ -1,36 +1,31 @@
-import ConfigParser as configparser
-# Renamed to that in Python3, may as well start now
-
+import json
 import io
 
 
 # This is the example configuration for an example ship
 # Most of these stats may or may not make any sense as we work
 # on the physics system. But it should indicate the vague feel of it
-exampleconfig = """
-[coreship]
-classname = Striker
-race = foobar
-mass = 5000
-engines = engine1,engine2
-guns = gun1,
-
-[engine1]
-placement = (10,-50,0)
-thrust = 2000
-warmuptime = 10
-
-[engine2]
-placement = (-10,-50,0)
-thrust = 2000
-warmuptime = 10
-
-[gun1]
-type = massdriver
-mass = 5
-damage = 50
-"""
-
+_exampleconfiguration = {
+    "classname":"Striker",
+    "race":["foobeings","barbeings"],
+    "mass":5000,
+    "engines":[
+        {"placement":(10,-50,0),
+         "thrust":2000,
+         "warmuptime":10,
+        },
+        {"placement":(-10,-50,0),
+         "thrust":2000,
+         "warmuptime":10,
+        }
+    ],
+    "guns":[
+        {"type":"massdriver",
+         "mass":5,
+         "damage":50
+        }
+    ],
+}
 class Ship(object):
     """The basic core ship class in Sidereal."""
     def __init__(self):
